d074ce7e92590130c22b149dcda2b57d269d7cdd
Lucas Block <lucas.blockv@gmail.com>
2022-07-31 16:48:34 -0300

    remove venv

--- gym-environments/src/space_invaders/neat.py
+++ gym-environments/src/space_invaders/neat.py
@@ -1,9 +1,16 @@
+import neat
+
 class NeatTraining:
-    pass
     def __init__(self, parameters = None, configuration=None):
         self.max_generations = parameters.generations
         self.actual_generation = 0
         self.configuration = configuration
-        
+        self.neat = neat.Config(
+            neat.DefaultGenome,
+            neat.DefaultReproduction,
+            neat.DefaultSpecieSet,
+            neat.DefaultStagnation,
+            './configs/config-neat'
+        )
     def get_action(self):
         pass

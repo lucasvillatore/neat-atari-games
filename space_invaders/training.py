e20ea96fe63aabc2f455b98e0ff70977143802b5
Lucas Block <lucas.blockv@gmail.com>
2022-08-23 18:43:15 -0300

    teste

--- space_invaders/training.py
+++ space_invaders/training.py
@@ -1,15 +1,19 @@
 import argparse
 import os
 import multiprocessing
+from random import randrange
 import cv2
 import neat
 import gym
 import numpy as np
 from datetime import datetime
+import imutils
 
-GAME = 'SpaceInvaders-v0'
+
+GAME = 'ALE/SpaceInvaders-v5'
 actions = {0:'NOOP', 1:'FIRE', 2:'RIGHT', 3:'LEFT', 4:'RIGHTFIRE', 5:'LEFTFIRE'}
-
+kernel = np.ones((5,5),np.uint8)
+INPUTS = 100
 def get_parameters():
     parser = argparse.ArgumentParser(description='Training to play space invaders')
     parser.add_argument('--generations', type=int, help='Number of generations', required=True)
@@ -30,16 +34,43 @@ def train_genome(genome, configuration):
     
     dim = (width, height)
     neat_network = neat.nn.feed_forward.FeedForwardNetwork.create(genome, configuration)
+    info = {"frame_number": 0}
+    action = None
     while not done:
+        if info["frame_number"] % 3 == 0 and action is not None:
+            n_state, reward, done, info = env.step(action)
+            score += reward
+            continue
+
         logger('Changing to gray')
         img = cv2.cvtColor(n_state, cv2.COLOR_BGR2GRAY)
-        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-        logger('After change')
-        
-        imgarray = np.ndarray.flatten(resized)
+        blurred = cv2.GaussianBlur(img, (5, 5), 0)
+        thresh = cv2.threshold(blurred, 20, 255, cv2.THRESH_BINARY)[1]
+
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+
+        all_tmp = []
+        for c in cnts:
+            # compute the center of the contour
+            M = cv2.moments(c)
+            try:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                all_tmp.append(cX)
+                all_tmp.append(cY)
+            except Exception as err:
+                print(M)
+                print(c)
+
+
+        if len(all_tmp) < INPUTS:
+            for i in range(0, INPUTS-len(all_tmp)):
+                all_tmp.append(0)
         
         logger('Activating network')
-        ai_decision = neat_network.activate(imgarray)
+        ai_decision = neat_network.activate(all_tmp)
         logger('After activate')
         
         action = np.argmax(ai_decision)
@@ -48,7 +79,7 @@ def train_genome(genome, configuration):
         
         n_state, reward, done, info = env.step(action)
         score += reward
-    fitness = score
+    fitness =info['lives'] * 20 + info['episode_frame_number']*0.001
 
     logger('Score: {}'.format(fitness), True)
     env.close()
@@ -75,43 +106,66 @@ if __name__ == '__main__':
     population.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     population.add_reporter(stats)
-    population.add_reporter(neat.Checkpointer(5))
+    population.add_reporter(neat.Checkpointer(25))
     current_time = datetime.now()
     logger('Running train_genomes', True)
     pe = neat.ParallelEvaluator(multiprocessing.cpu_count(), train_genome)
-    winner = population.run(pe.evaluate, 20)
+    winner = population.run(pe.evaluate, 200)
     
     winner_net = neat.nn.FeedForwardNetwork.create(winner, neat_configuration)
 
     env = gym.make(GAME, render_mode='human')
-    state = env.reset()
+
+    n_state = env.reset()
     done = False
 
     score = 0
 
-    height, width, channels = state.shape 
-    
-    height = int(height * 50 / 100)
-    width = int(width * 50 / 100)
-    
-    dim = (width, height)
-
+    info = {"frame_number": 0}
+    action = None
     while not done:
+
+        if info["frame_number"] % 3 == 0 and action is not None:
+            n_state, reward, done, info = env.step(action)
+            score += reward
+            continue
+
         logger('Changing to gray')
         img = cv2.cvtColor(n_state, cv2.COLOR_BGR2GRAY)
-        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-        logger('After change')
-        
-        imgarray = np.ndarray.flatten(resized)
+        blurred = cv2.GaussianBlur(img, (5, 5), 0)
+        thresh = cv2.threshold(blurred, 20, 255, cv2.THRESH_BINARY)[1]
+
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+
+        all_tmp = []
+        for c in cnts:
+            # compute the center of the contour
+            M = cv2.moments(c)
+            try:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                all_tmp.append(cX)
+                all_tmp.append(cY)
+            except Exception as err:
+                print(M)
+
+
+        if len(all_tmp) < INPUTS:
+            for i in range(0, INPUTS-len(all_tmp)):
+                all_tmp.append(0)
+            
+            
         
         logger('Activating network')
-        ai_decision = winner_net.activate(imgarray)
+        # ai_decision = winner_net.activate(imgarray)
         logger('After activate')
         
-        action = np.argmax(ai_decision)
-        logger(f"Action: {actions[action]}")
+        # action = np.argmax(ai_decision)
+        action = randrange(6)
+        logger(f"Action: {actions[action]}", True)
+        
         
         n_state, reward, done, info = env.step(action)
         score += reward
-
-    logger('Score: {}'.format(score), True)

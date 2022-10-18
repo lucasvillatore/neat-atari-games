433d7f18bb844d0793ff714d9fa9c9877fc55fb6
Lucas Block <lucas.blockv@gmail.com>
2022-10-17 22:52:32 -0300

    add script run all

--- run_all.py
+++ run_all.py
@@ -4,7 +4,12 @@ import os
 if __name__ == '__main__':
 
     games = [
-        {"game": "SpaceInvaders-ram-v0", "config": "./space_invaders/configs/neat-config", 'folder': './space_invaders'}
+        {"game": "ALE/MsPacman-ram-v5", "config": "./pacman/configs/neat-config", 'folder': './pacman'},
+        # {"game": "Pendulum-v1", "config": "./pendulum/configs/neat-config", 'folder': './pendulum'},
+        {"game": "MountainCar-v0", "config": "./mountain_car/configs/neat-config", 'folder': './mountain_car'},
+        {"game": "CartPole-v1", "config": "./cart_pole/configs/neat-config", 'folder': './cart_pole'},
+        {"game": "Acrobot-v1", "config": "./acrobot/configs/neat-config", 'folder': './acrobot'},
+        {"game": "SpaceInvaders-ram-v0", "config": "./space_invaders/configs/neat-config", 'folder': './space_invaders'},
     ]
 
 

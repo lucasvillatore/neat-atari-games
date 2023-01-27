111938e4adf47f2ee9f05fb5e4a9056cfa5cd76d
Lucas Block <lucas.blockv@gmail.com>
2023-01-26 22:47:59 -0300

    remove codigo não usado

--- src/run_all.py
+++ src/run_all.py
@@ -1,9 +1,5 @@
 
 from trainer import run
-# from pacman.interface import Pacman
-# from alien.interface import Alien
-# from space_invaders.interface import SpaceInvaders
-# from assault.interface import Assault
 from pong.interface import Pong
 from breakout.interface import Breakout
 from tennis.interface import Tennis
@@ -12,7 +8,7 @@ from skiing.interface import Skiing
 if __name__ == '__main__':
 
     games = [
-        Skiing(net='./pong/network/winner.pkl'),
+        Skiing(),
         # Breakout(net='./pong/network/winner.pkl'),
         # Pong(),
         # Tennis()

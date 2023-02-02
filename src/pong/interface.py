ece6f0548385e91bc13f2bfaf50788998f0dd790
lbv17 <lbv17@inf.ufpr.br>
2023-02-02 17:46:11 -0300

    add pong testes

--- src/pong/interface.py
+++ src/pong/interface.py
@@ -20,21 +20,21 @@ class Pong():
 
         if abs(int(info['labels']['player_x']) - int(info['labels']['ball_x'])) < 10:
             reward += 1 * 0.03
-        
+
         if abs(int(info['labels']['player_y']) - int(info['labels']['ball_y'])) < 10:
             reward += 1 * 0.05
 
         return reward
 
     def get_distance(self, player_x, player_y, ball_x, ball_y):
-        distance = math.sqrt(math.pow(player_x - ball_x, 2) + math.pow(player_y - ball_y, 2)) 
-        
+        distance = math.sqrt(math.pow(player_x - ball_x, 2) + math.pow(player_y - ball_y, 2))
+
         return distance
 
     def get_action(self, observation_space, net, step, info):
 
         is_first_action = step == 0
-        
+
         if is_first_action:
             return 0
 
@@ -50,9 +50,9 @@ class Pong():
             action = np.argmax(output)
         except Exception as err:
             action = 0
-        
+
         return action
-        
+
     def run(self, net, env, steps):
 
         observation_space = env.reset()
@@ -73,9 +73,10 @@ class Pong():
 
             if done:
                 break
-        
-        total_reward += game_information['labels']['enemy_score'] * -1 * 2 + game_information['labels']['player_score'] * 2
-        
+
+        total_reward += game_information['labels']['enemy_score'] * -1 * 5 + game_information['labels']['player_score'] * 2
+        total_reward += game_information['frame_number'] * 0.0001
+
         if total_reward < 0:
             total_reward = 0
         return total_reward
